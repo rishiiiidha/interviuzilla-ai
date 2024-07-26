4a37f334788068173a27b59c0ec36f2dd3b2e382
rishiiiidha <rishidha04@gmail.com>
2024-07-26 11:51:30 +0530

    small changes

--- app/_components/Button.jsx
+++ app/_components/Button.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Button({ label }) {
   return (
-    <button className="flex flex-col justify-center self-stretch text-xs font-medium leading-4">
-      <span className="px-5 py-4 bg-indigo-600 rounded-[50px]">
-        {label}
-      </span>
-    </button>
+    <a href="/dashboard">
+      <button className="flex flex-col justify-center self-stretch text-xs font-medium leading-4">
+        <span className="px-5 py-4 bg-indigo-600 rounded-[50px]">{label}</span>
+      </button>
+    </a>
   );
 }
 
--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,8 +1,9 @@
-"use client"
+"use client";
 import React from "react";
 import NavItem from "./NavItem";
 import Button from "./Button";
 import { UserButton } from "@clerk/nextjs";
+import { Link, NavLink } from "react-router-dom";
 
 const navItems = [
   { label: "Dashboard", href: "/dashboard" },
@@ -16,15 +17,17 @@ function Header() {
       <div className="flex justify-center items-center px-8 w-full max-md:px-5 max-md:max-w-full">
         <div className="flex flex-col justify-center w-full max-w-screen-xl max-md:max-w-full">
           <nav className="flex gap-5 justify-between mt-4 w-full max-md:flex-wrap max-md:max-w-full">
-            <h1 className="flex flex-col justify-center my-auto text-2xl font-semibold leading-8 bg-clip-text text-white">
-              Interviuzilla AI
-            </h1>
+            <a href="/">
+              <h1 className="flex flex-col justify-center my-auto text-2xl font-semibold leading-8 bg-clip-text text-white">
+                Interviuzilla AI
+              </h1>
+            </a>
             <ul className="flex gap-5 justify-between items-center text-sm leading-5 text-white  max-md:flex-wrap">
               {navItems.map((item, index) => (
                 <NavItem key={index} label={item.label} href={item.href} />
               ))}
               <li className="ml-2">
-                <UserButton/>
+                <UserButton />
               </li>
             </ul>
           </nav>
